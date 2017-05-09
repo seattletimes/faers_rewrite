339d60defe1de84b36eb0fea5fd4755207700daf
Thomas Wilburn <news@thomaswilburn.net>
2017-05-09 12:28:47 -0700

    Split file normalization into a separate step

--- 2_scrape_faers.js
+++ 2_scrape_faers.js
@@ -1,6 +1,5 @@
 var async = require("async");
 var cheerio = require("cheerio");
-var csv = require("csv");
 var request = require("request");
 var shell = require("shelljs");
 
@@ -60,82 +59,4 @@ var unzipCurrent = function(done = noop) {
   });
 };
 
-/*
-
-Now we need to merge the files together into one big file per table: DEMO,
-DRUG, INDI, OUTC, REAC, RPSR, and THER.
-
-Fun problems: the filenames are not consistently cased up or down. There are
-multiple formats per individual file type, which we will want to sniff out
-from the header. These files are too large to want to load and process in
-their entirety. So we'll use a stream to load them and then serialize them
-back out into our single-data files.
-
-Stream handlers are loaded from clean_current and clean_legacy, and should
-take the form of a transform function that takes the input from each file and
-converts it into an object with the expected columns. The function should also
-have an attached `columns` property containing an array with the order of the
-columns for output.
-
-*/
-
-var processCurrent = function() {
-  var cleaners = require("./utils/cleaners");
-
-  var files = fs.readdirSync("scratch/ascii");
-
-  async.eachSeries(files, function(f, nextFile) {
-
-    var prefix = f.slice(0, 4).toLowerCase();
-    var basename = path.basename(f, path.extname(f)).toUpperCase();
-    var cleaner = cleaners[prefix];
-
-    //create the output for the normalized file
-    shell.mkdir("-p", `scratch/normalized/${prefix}`);
-    var fileStream = fs.createWriteStream(`scratch/normalized/${prefix}/${basename}.csv`);
-    var csvStream = csv.stringify({ headers: true, columns: cleaner.columns });
-    csvStream.pipe(fileStream);
-
-    var parser = csv.parse({
-      columns: true,
-      delimiter: "$",
-      relax: true
-    });
-
-    console.time(f);
-
-    parser.on("error", err => console.log(err));
-
-    parser.on("data", row => csvStream.write(cleaner(row)));
-
-    parser.on("finish", function(event) {
-      console.log(`Processed ${parser.count} rows`);
-      console.timeEnd(f);
-      if (global.gc) global.gc();
-      csvStream.end();
-      fileStream.end();
-      nextFile();
-    });
-
-    console.log(`Processing ${f} using type "${prefix}"...`);
-    var input = fs.createReadStream(path.join("scratch/ascii", f));
-    input.pipe(parser);
-
-  }, function() {
-
-    console.log("All files done");
-
-  });
-
-};
-
-var combineCurrent = function(done = noop) {
-  var normalized = fs.readdirSync("scratch/normalized");
-  async.eachSeries(normalized, function(n, c) {
-    var files = fs.readdirSync(`scratch/normalized/${n}`);
-    var command = files.join(" ");
-    shell.exec(`cat ${command} > ../combined/${n}.csv`, { cwd: "scratch/normalized" }, c);
-  }, done);
-}
-
-async.series([/*getCurrentFaers, unzipCurrent,*/ processCurrent, combineCurrent]);
+async.series([getCurrentFaers, unzipCurrent]);
---
+++ 3_normalize_and_combine.js
@@ -0,0 +1,84 @@
+var async = require("async");
+var csv = require("csv");
+var shell = require("shelljs");
+
+var fs = require("fs");
+var path = process.platform == "win32" ? require("path").win32 : require("path");
+
+var noop = function() {};
+
+/*
+
+Now we need to merge the files together into one big file per table: DEMO,
+DRUG, INDI, OUTC, REAC, RPSR, and THER.
+
+Fun problems: the filenames are not consistently cased up or down. There are
+multiple formats per individual file type, which we will want to sniff out
+from the header. These files are too large to want to load and process in
+their entirety. So we'll use a stream to load them and then serialize them
+back out into our single-data files.
+
+Stream handlers are loaded from clean_current and clean_legacy, and should
+take the form of a transform function that takes the input from each file and
+converts it into an object with the expected columns. The function should also
+have an attached `columns` property containing an array with the order of the
+columns for output.
+
+*/
+
+var processCurrent = function(done = noop) {
+  var cleaners = require("./utils/cleaners");
+
+  var files = fs.readdirSync("scratch/ascii");
+
+  async.eachSeries(files, function(f, nextFile) {
+
+    var prefix = f.slice(0, 4).toLowerCase();
+    var basename = path.basename(f, path.extname(f)).toUpperCase();
+    var cleaner = cleaners[prefix];
+
+    //create the output for the normalized file
+    shell.mkdir("-p", `scratch/normalized/${prefix}`);
+    var fileStream = fs.createWriteStream(`scratch/normalized/${prefix}/${basename}.csv`);
+    var csvStream = csv.stringify({ headers: true, columns: cleaner.columns });
+    csvStream.pipe(fileStream);
+
+    var parser = csv.parse({
+      columns: true,
+      delimiter: "$",
+      relax: true
+    });
+
+    console.time(f);
+
+    parser.on("error", err => console.log(err));
+
+    parser.on("data", row => csvStream.write(cleaner(row)));
+
+    parser.on("finish", function(event) {
+      console.log(`Processed ${parser.count} rows`);
+      console.timeEnd(f);
+      if (global.gc) global.gc();
+      csvStream.end();
+      fileStream.end();
+      nextFile();
+    });
+
+    console.log(`Processing ${f} using type "${prefix}"...`);
+    var input = fs.createReadStream(path.join("scratch/ascii", f));
+    input.pipe(parser);
+
+  }, done);
+
+};
+
+var combineCurrent = function(done = noop) {
+  var normalized = fs.readdirSync("scratch/normalized");
+  async.eachSeries(normalized, function(n, c) {
+    var files = fs.readdirSync(`scratch/normalized/${n}`);
+    var command = files.join(" ");
+    shell.exec(`cat ${command} > ../combined/${n}.csv`, { cwd: "scratch/normalized" }, c);
+  }, done);
+}
+
+async.series([processCurrent, combineCurrent]);
